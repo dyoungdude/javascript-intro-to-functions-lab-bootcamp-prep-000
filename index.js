7e9f1dfb88650cee929048d06927aebc6dea1861
Darren Young <dlyoung5@asu.edu>
2018-07-13 17:07:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log('HELLO')
+    function spy(spyOn){
+      spyOn(logShout)
+      return spyOn.toLowerCase()
+    }
+  return string.toUpperCase()
+}
