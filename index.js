706055a89a9d6b10914f4ad21d5e4f0e7be31a97
Darren Young <dlyoung5@asu.edu>
2018-07-13 18:13:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if(sayHiToGrandma === 'hello'){
+  if(string === 'hello'){
     return('I can\'t hear you!')
-  }else if(sayHiToGrandma === 'HELLO'){
+  }else if(string === 'HELLO'){
     return('YES INDEED!')
-  }else if(sayHiToGrandma === 'I love you, Grandma.'){
+  }else if(string === 'I love you, Grandma.'){
     return('I love you, too.')
   }else{
     console.log("null")
